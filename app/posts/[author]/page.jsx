196a073e42b6dcd19c6256061962bd22c401c890
viktor-drok <vityadrok@gmail.com>
2023-09-03 18:37:14 +0300

    card action on post

--- app/posts/[author]/page.jsx
+++ app/posts/[author]/page.jsx
@@ -1,4 +1,5 @@
-import { Card, CardContent, Grid, Typography } from "@mui/material";
+import AddComment from "@/app/components/AddComment";
+import { Box, Card, CardActions, CardContent, Grid, Typography } from "@mui/material";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 const page = async ({ params }) => {
@@ -10,12 +11,17 @@ const page = async ({ params }) => {
 		<Grid container gap={2} my={5} mx={5}>
 			{posts?.map(post => {
 				return (
-					<Card key={post.id} sx={{ width: 400, maxHeight: 350, overflowY: "auto" }}>
-						<CardContent>
-							<Typography variant="h4">{post.title}</Typography>
-							<Typography variant="body2">{post.post}</Typography>
-						</CardContent>
-					</Card>
+					<Box key={post.id} sx={{ position: "relative" }}>
+						<Card sx={{ width: 400, maxHeight: 350, overflowY: "auto" }}>
+							<CardContent>
+								<Typography variant="h4">{post.title}</Typography>
+								<Typography variant="body2">{post.post}</Typography>
+							</CardContent>
+							<CardActions>
+								<AddComment />
+							</CardActions>
+						</Card>
+					</Box>
 				);
 			})}
 		</Grid>
