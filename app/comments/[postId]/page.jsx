a49fdee34ad8aba21b6d4e81a3cb7730a51daed3
viktor-drok <vityadrok@gmail.com>
2023-09-05 20:46:06 +0300

    suspence for comments

--- app/comments/[postId]/page.jsx
+++ app/comments/[postId]/page.jsx
@@ -7,10 +7,14 @@ import { Box } from "@mui/material";
 
 const Comment = () => {
 	const { postId } = useParams();
-	const { data: comments } = useQuery({
+	const { data: comments, isFetching } = useQuery({
 		queryKey: ["comment"],
 		queryFn: () => getComment(postId),
 	});
+
+	if (isFetching) {
+		return <div>Loading...</div>;
+	}
 	return (
 		<Box px={4}>
 			{comments?.map(item => (
